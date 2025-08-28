7345429c5178dfb27800961f72db34ac53c1c868
은서 <supreme327@naver.com>
2025-08-28 11:58:05 +0900

    Margin ruler

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,32 +1,34 @@
 "use client";
 import StarterKit from '@tiptap/starter-kit'
-import { TableKit } from '@tiptap/extension-table'
+import {TableKit} from '@tiptap/extension-table'
 import Underline from '@tiptap/extension-underline'
-import { TextStyle, FontFamily } from '@tiptap/extension-text-style'
+import {TextStyle, FontFamily} from '@tiptap/extension-text-style'
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
-import { Color } from '@tiptap/extension-color'
+import {Color} from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
-import { TaskItem, TaskList } from '@tiptap/extension-list'
+import {TaskItem, TaskList} from '@tiptap/extension-list'
 import {useEditor, EditorContent} from '@tiptap/react'
 import {useEditorStore} from "@/store/use-editor-store";
 import {FontSizeExtension} from "@/extensions/font-size";
 import {LineHeightExtension} from "@/extensions/line-height";
+import {Ruler} from "./ruler";
 
 export const Editor = () => {
     const {setEditor} = useEditorStore();
 
     const editor = useEditor({
+        immediatelyRender: false,
         onCreate({editor}) {
             setEditor(editor);
         },
         onDestroy() {
-          setEditor(null);
+            setEditor(null);
         },
         onSelectionUpdate({editor}) {
-          setEditor(editor);
+            setEditor(editor);
         },
         onTransaction({editor}) {
             setEditor(editor);
@@ -41,7 +43,7 @@ export const Editor = () => {
             setEditor(editor);
         },
         editorProps: {
-            attributes:{
+            attributes: {
                 style: "padding-left: 56px; padding-right: 56px;",
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             },
@@ -51,7 +53,7 @@ export const Editor = () => {
             FontSizeExtension,
             LineHeightExtension,
             Link.configure({
-               openOnClick: false,
+                openOnClick: false,
                 autolink: true,
                 defaultProtocol: "https",
             }),
@@ -95,6 +97,7 @@ export const Editor = () => {
     return (
         <div
             className="w-full h-full overflow-x-auto bg-[#FAFBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <Ruler/>
             <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
                 <EditorContent editor={editor}/>
             </div>
